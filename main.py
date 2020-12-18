be56f77c1ae6914e05725a04a9cf37dc758a6892
Juan Cruz <jc23729@gmail.com>
2020-12-18 22:38:18 +0000

    T

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from random import randint
 from art import logo
 
+#Choosing a random number between 1 and 100
+
+
+#make a function to set difficulty
+
+
+#Let the user guess a number
+
+
+#Check that number that they guess to origonal number that we had, So probably a function to guess actual answer 
+
+
+# track the number of turns and reduce by wrong if they get it wrong
+
+#Repeat the guessing functionality 
